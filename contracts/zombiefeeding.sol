c83f1c02b4cf21667eb91df476ccff4bb57063fe
Nick Yang <nick10811@gmail.com>
2022-07-09 13:48:56 +0800

    feedAndMultiply in ZombieFeeding

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.8.0;
 
-import "./zombiefactory.sol"
+import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId]; // store in the storage
+      _targetDna = _targetDna % dnaModulus;
+      uint newDna = (myZombie.dna + _targetDna) / 2;
+      _createZombie("NoName", newDna);
+  }
 }
