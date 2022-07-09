840df8b79f83447c519faa21e542cda2fb0ce233
Nick Yang <nick10811@gmail.com>
2022-07-09 12:43:52 +0800

    msg.sender

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -13,13 +13,16 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
-    
+
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // msg.sender is a global variable that refers to an address
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
