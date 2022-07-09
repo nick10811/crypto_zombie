24a5f8c5fad2f3a8b41eee1c242ae3c3e48e59cd
Nick Yang <nick10811@gmail.com>
2022-07-09 12:39:38 +0800

    mapping (key => value)

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -13,6 +13,10 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+    
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
 
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
