53c48141426331d3048c7f57bb5ff0bdc7afa71b
Nick Yang <nick10811@gmail.com>
2023-06-14 14:54:49 +0800

    Implement a skeleton of ZombieOwnership with ERC721

---
+++ contracts/zombieownership.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    function balanceOf(address _owner) external view returns (uint256) {
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+    }
+
+    function transferFrom(_from, _to, _tokenId) external payable {
+    }
+
+    function approve(_approved, _tokenId) external payable  {
+    }
+
+}
