2221d30a2250aaedd4d63eb246f7028773d26602
Nick Yang <nick10811@gmail.com>
2023-06-14 15:57:41 +0800

    implement transferFrom()

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -4,6 +4,7 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    mapping(uint => address) zombieApprovals;
 
     function balanceOf(address _owner) external view returns (uint256) {
         return ownerZombieCount[_owner];
@@ -14,16 +15,19 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     }
 
     function _transfer(address _from, address _to, uint256 _tokenId) private {
-      ownerZombieCount[_to]++;
-      ownerZombieCount[_from]--;
-      zombieToOwner[_tokenId] = _to;
-      emit Transfer(_from, _to, _tokenId);
-  }
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
 
     function transferFrom(_from, _to, _tokenId) external payable {
+        require(
+            zombieToOwner[_tokenId] == msg.sender ||
+                zombieApprovals[_tokenId] == msg.sender
+        );
+        _transfer(_from, _to, _tokenId);
     }
 
-    function approve(_approved, _tokenId) external payable  {
-    }
-
+    function approve(_approved, _tokenId) external payable {}
 }
