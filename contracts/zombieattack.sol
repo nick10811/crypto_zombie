ae7ded3b69dd7485952b0315e94a56645be9ef86
Nick Yang <nick10811@gmail.com>
2023-06-14 14:16:25 +0800

    implement zombie wins and losses

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -17,5 +17,16 @@ contract ZombieAttack is ZombieHelper {
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
         uint rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            // our zombie wins
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
     }
 }
