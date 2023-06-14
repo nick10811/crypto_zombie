1804da916683252545dd99dcc2837ac81c14dd06
Nick Yang <nick10811@gmail.com>
2023-06-14 13:54:06 +0800

    add modifier ownerOf()

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -19,6 +19,12 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract;
 
+    modifier ownerOf(uint _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        _;
+    }
+
+    }
     function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
    }
@@ -31,8 +37,7 @@ contract ZombieFeeding is ZombieFactory {
   function _isReady(Zombie storage _zombie) internal view returns(bool) {
       return (_zombie.readyTime <= now);
   }
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-      require(msg.sender == zombieToOwner[_zombieId]);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
       Zombie storage myZombie = zombies[_zombieId]; // store in the storage
       require(_isReady(myZombie)); // check myZombie is ready
       _targetDna = _targetDna % dnaModulus;
--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -27,16 +27,14 @@ contract ZombieHelper is ZombieFeeding {
     function changeName(
         uint _zombieId,
         string calldata _newName
-    ) external aboveLevel(2, _zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    ) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
         zombies[_zombieId].name = _newName;
     }
 
     function changeDna(
         uint _zombieId,
         uint _newDna
-    ) external aboveLevel(20, _zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    ) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
         zombies[_zombieId].dna = _newDna;
     }
 
